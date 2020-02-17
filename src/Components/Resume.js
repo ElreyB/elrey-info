605af5b1d1ef8df0265bcc9cc5bfe9d0157ca40a
Elrey Belmonti <elbelmonti@gmail.com>
2020-02-17 01:48:20 -0800

    fix row bug

--- src/Components/Resume.js
+++ src/Components/Resume.js
@@ -36,16 +36,16 @@ class Resume extends Component {
 
     return (
       <section id="resume">
-        <div className="row education">
-          <div className="row work">
-            <div className="three columns header-col">
-              <h1>
-                <span>Work</span>
-              </h1>
-            </div>
-
-            <div className="nine columns main-col">{work}</div>
+        <div className="row work">
+          <div className="three columns header-col">
+            <h1>
+              <span>Work</span>
+            </h1>
           </div>
+
+          <div className="nine columns main-col">{work}</div>
+        </div>
+        <div className="row education">
           <div className="three columns header-col">
             <h1>
               <span>Education</span>
