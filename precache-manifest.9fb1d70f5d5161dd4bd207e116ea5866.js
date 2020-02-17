3b88349aa19b9a5e8c2273f1f86bc258bf5850a0
Elrey Belmonti <elbelmonti@gmail.com>
2020-02-17 00:59:57 -0800

    Updates

---
+++ precache-manifest.9fb1d70f5d5161dd4bd207e116ea5866.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5738e5afb0630359d014f15185cc81a",
+    "url": "/elrey-info/index.html"
+  },
+  {
+    "revision": "973e447e036b12342a69",
+    "url": "/elrey-info/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "19820b40d2a9e72be11f",
+    "url": "/elrey-info/static/js/2.1527cff5.chunk.js"
+  },
+  {
+    "revision": "6e498969aafde287c0e2e1a721426fda",
+    "url": "/elrey-info/static/js/2.1527cff5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "973e447e036b12342a69",
+    "url": "/elrey-info/static/js/main.d3894478.chunk.js"
+  },
+  {
+    "revision": "a5d39fd0341799e373c7",
+    "url": "/elrey-info/static/js/runtime-main.768fa861.js"
+  }
+]);
